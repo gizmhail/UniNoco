168e9bbd9a8cae5da6265a7f40d0815d57d32953
Gizmhail <gizmhail@gmail.com>
2017-04-22 00:56:23 +0200

    Add configuration file

---
+++ Assets/Scripts/Conf/NocoConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NocoVR
+{
+	public static class NocoConfiguration
+	{
+		public const string clientId = "";
+		public const string clientSecret = "";
+	}
+
+	public static class NocoDevConfiguration
+	{
+		public const string clientId = "";
+		public const string clientSecret = "";
+		public const string testUser = "";
+		public const string testPassword = "";
+	}
+}
